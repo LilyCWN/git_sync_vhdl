b857f6e66c83cc2ef8d8ff4450df1f392681f9f5
john <soul07770@gmail.com>
2018-12-02 18:22:46 +0800

    added adder.hdl

--- sources_1/new/top.vhd
+++ sources_1/new/top.vhd
@@ -23,21 +23,13 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity top is
     Port ( 
 		clk_in1_p : in STD_LOGIC;
 		clk_in1_n : in STD_LOGIC;
 		rst: in std_logic;
-		add_out: out std_logic_vector(1 downto 0)
+		add_out: out std_logic_vector(1 downto 0);
+		add_out2: out std_logic_vector(1 downto 0)
 		);
 end top;
 
@@ -67,6 +59,14 @@ component c_addsub_0
   );
 END component;
 
+component adder
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           S : out STD_LOGIC_VECTOR (1 downto 0));
+end component;
+
 signal clk: std_logic;
 signal addr: std_logic_vector(0 downto 0);
 signal rom_out: std_logic_vector(0 downto 0);
@@ -88,6 +88,14 @@ c: c_addsub_0 port map (
 	B => "1",
 	CLK => clk,
 	S => add_out
+);
+
+d: adder port map (
+    clk => clk,
+    rst => rst,
+    A => rom_out(0),
+    B => '1',
+    S => add_out2
 );
 
 process(clk,rst)
